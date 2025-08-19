c9779c39671d817553d7ae327ec982efecc46925
th3nu8 <blueboltgamingyt@gmail.com>
2025-08-18 19:08:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,27 +8,32 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static files from the Public folder
+// Serve static files from Public
 app.use(express.static(path.join(__dirname, 'Public')));
 
-// Store block data: {x|y|z: {x, y, z, color}}
+// Ensure / sends index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Public', 'index.html'));
+});
+
+// Block storage: key = "x|y|z", value = {x, y, z, color}
 const blocks = {};
 
-// Handle socket connections
+// Socket.IO connections
 io.on('connection', socket => {
   console.log('A user connected:', socket.id);
 
-  // Send current blocks to new player
+  // Send current blocks
   socket.emit('init', { blocks: Object.values(blocks) });
 
-  // When a player builds a block
+  // Build block
   socket.on('build', data => {
     const key = `${data.x}|${data.y}|${data.z}`;
     blocks[key] = { x: data.x, y: data.y, z: data.z, color: data.color || 0xffffff };
     io.emit('build', blocks[key]);
   });
 
-  // When a player removes a block
+  // Remove block
   socket.on('remove', data => {
     const key = `${data.x}|${data.y}|${data.z}`;
     delete blocks[key];
