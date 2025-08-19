f95717774b9c4620269107ba9d58515e94419440
th3nu8 <blueboltgamingyt@gmail.com>
2025-08-18 19:00:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,71 +1,84 @@
+// server.js
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const PORT = process.env.PORT || 3000;
+// Serve the Public folder
+app.use(express.static(__dirname + '/Public'));
 
-// Serve static files (index.html, textures, client scripts)
-app.use(express.static(__dirname + '/'));
-
-// Track connected players
-const players = {}; // playerId -> {x, y, z, yaw, name}
-
-// Track built blocks (optional for new players joining)
-const blocks = []; // each block: {x, y, z, color}
-
-// Socket.IO events
-io.on('connection', socket => {
-  console.log('Player connected:', socket.id);
-
-  // Player joins
-  socket.on('join', data => {
-    players[socket.id] = data;
-
-    // Send current world state to the new player
-    socket.emit('init', {
-      blocks: blocks,
-      players
-    });
-  });
-
-  // Player movement
-  socket.on('move', data => {
-    if (players[socket.id]) {
-      players[socket.id] = { ...players[socket.id], ...data };
-      socket.broadcast.emit('player-update', { id: socket.id, ...data });
-    }
-  });
-
-  // Build a block
-  socket.on('build', data => {
-    // data = {x, y, z, color}
-    blocks.push(data);           // store block
-    socket.broadcast.emit('build', data); // notify others
-  });
-
-  // Remove a block
-  socket.on('remove', data => {
-    // Remove block from server list
-    const index = blocks.findIndex(b => b.x === data.x && b.y === data.y && b.z === data.z);
-    if (index !== -1) blocks.splice(index, 1);
-
-    socket.broadcast.emit('remove', data);
-  });
-
-  // Name change
-  socket.on('name-change', data => {
-    if (players[socket.id]) players[socket.id].name = data.name;
-    socket.broadcast.emit('player-update', { id: socket.id, name: data.name });
-  });
-
-  // Player disconnects
-  socket.on('disconnect', () => {
-    console.log('Player disconnected:', socket.id);
-    delete players[socket.id];
-    socket.broadcast.emit('player-leave', { id: socket.id });
-  });
+// Serve index.html explicitly
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/Public/index.html');
 });
 
-// Start server
+// ---- GAME STATE ----
+const blocks = new Map(); // key: "x|y|z" -> { x, y, z }
+const players = {};       // playerId -> { x, y, z, name }
+
+// Helper to get key from coordinates
+function keyOf(x, y, z) {
+    return `${x}|${y}|${z}`;
+}
+
+// ---- SOCKET.IO ----
+io.on('connection', socket => {
+    console.log(`Player connected: ${socket.id}`);
+
+    // Send current blocks & players to the new client
+    socket.emit('init', {
+        blocks: Array.from(blocks.values()),
+        players
+    });
+
+    // When a player joins
+    socket.on('join', data => {
+        players[socket.id] = { x: data.x, y: data.y, z: data.z, name: data.name };
+        socket.broadcast.emit('player-join', { id: socket.id, ...players[socket.id] });
+    });
+
+    // When a player moves
+    socket.on('move', data => {
+        if (players[socket.id]) {
+            players[socket.id] = { ...players[socket.id], ...data };
+            socket.broadcast.emit('player-update', { id: socket.id, ...data });
+        }
+    });
+
+    // When a block is built
+    socket.on('build', data => {
+        const k = keyOf(data.x, data.y, data.z);
+        if (!blocks.has(k)) {
+            blocks.set(k, { x: data.x, y: data.y, z: data.z, color: data.color || 0x00ff00 });
+            io.emit('build', blocks.get(k)); // broadcast to all
+        }
+    });
+
+    // When a block is removed
+    socket.on('remove', data => {
+        const k = keyOf(data.x, data.y, data.z);
+        if (blocks.has(k)) {
+            blocks.delete(k);
+            io.emit('remove', data);
+        }
+    });
+
+    // When player changes name
+    socket.on('name-change', data => {
+        if (players[socket.id]) {
+            players[socket.id].name = data.name;
+            socket.broadcast.emit('player-update', { id: socket.id, name: data.name });
+        }
+    });
+
+    // When player disconnects
+    socket.on('disconnect', () => {
+        console.log(`Player disconnected: ${socket.id}`);
+        delete players[socket.id];
+        socket.broadcast.emit('player-leave', { id: socket.id });
+    });
+});
+
+// ---- START SERVER ----
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
