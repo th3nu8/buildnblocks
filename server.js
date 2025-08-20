be93346804094b878ad0cb979c1b95fca35cb004
th3nu8 <blueboltgamingyt@gmail.com>
2025-08-20 10:33:37 -0500

    nvm

--- server.js
+++ server.js
@@ -61,8 +61,9 @@ io.on('connection', (socket) => {
   })
 })
 
-const PORT = process.env.PORT || 443
+const PORT = process.env.PORT || 80
 server.listen(PORT, '0.0.0.0', () => console.log('Server running on port', PORT))
+
 
 
 
